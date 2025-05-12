79d36fab10f344531c2056b94f4283ec94b967e0
Ricc <ricochristian707@gmail.com>
2025-05-12 21:05:43 +0800

    final kayanya

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+	slidesPerView: 2,
+	direction: "horizontal",
+	loop: false,
+	spaceBetween: 2,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
