10ba1c90f64cae99a6eacff162d8a045774b821d
Ricc <ricochristian707@gmail.com>
2025-05-19 23:47:18 +0800

    last commit kayanya

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ const swiper = new Swiper(".swiper", {
 		clickable: true,
 	},
 });
+
+const swiper2 = new Swiper(".swiper2", {
+	slidesPerView: 2,
+	direction: "horizontal",
+	loop: false,
+	spaceBetween: 2,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
